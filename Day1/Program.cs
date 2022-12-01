4118598466eb54bf94b009bf2dd23427bf9582dd
Rick van den Berg <rick.vanden.berg@sogeti.com>
2022-12-01 13:16:10 +0100

    Add project files.

---
+++ Day1/Program.cs
@@ -0,0 +1,22 @@
+﻿List<string> lines = File.ReadAllLines("input1.txt").ToList();
+List<int> amounts = new();
+
+int amount = 0;
+foreach (string line in lines)
+{
+    if (string.IsNullOrEmpty(line))
+    {
+        amounts.Add(amount);
+        amount = 0;
+        continue;
+    }
+
+    amount += int.Parse(line);
+}
+
+amounts.Sort();
+amounts.Reverse();
+
+Console.WriteLine(amounts[0]);
+Console.WriteLine(amounts[0] + amounts[1] + amounts[2]);
+Console.ReadLine();
