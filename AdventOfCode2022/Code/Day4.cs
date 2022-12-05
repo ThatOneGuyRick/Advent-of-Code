21ecc87b6c867630e4a7d1a135f886c160712a19
Rick van den Berg <rick.vanden.berg@sogeti.com>
2022-12-05 10:44:03 +0100

    day 4 + 5

---
+++ AdventOfCode2022/Code/Day4.cs
@@ -0,0 +1,45 @@
+﻿using AdventOfCode;
+using AdventOfCode.Common;
+
+namespace AdventOfCode2022.Code
+{
+    internal class Day4 : IAdventOfCode
+    {
+        private List<(int elf1Start, int elf1End, int elf2Start, int elf2End)> _ranges = new();
+
+        public Day4()
+        {
+            foreach (string line in File.ReadAllLines("Input/day4.txt"))
+            {
+                string[] splits = line.SplitOn("-", ",", "-");
+                _ranges.Add((splits[0].ToInt(), splits[1].ToInt(), splits[2].ToInt(), splits[3].ToInt()));
+            }
+        }
+
+        public string PartOne()
+        {
+            int total = 0;
+            foreach ((int elf1Start, int elf1End, int elf2Start, int elf2End) in _ranges)
+            {
+                if ((elf1Start <= elf2Start && elf1End >= elf2End) || (elf2Start <= elf1Start && elf2End >= elf1End))
+                {
+                    total++;
+                }
+            }
+            return $"{total}";
+        }
+
+        public string PartTwo()
+        {
+            int total = 0;
+            foreach ((int elf1Start, int elf1End, int elf2Start, int elf2End) in _ranges)
+            {
+                if (elf1Start <= elf2End && elf1End >= elf2Start)
+                {
+                    total++;
+                }
+            }
+            return $"{total}";
+        }
+    }
+}
---
+++ AdventOfCode2022/Code/Day5.cs
@@ -0,0 +1,137 @@
+﻿using AdventOfCode;
+using AdventOfCode.Common;
+
+namespace AdventOfCode2022.Code
+{
+    internal class Day5 : IAdventOfCode
+    {
+        private List<(int amount, int from, int to)> _moves = new();
+        private CrateStack[] _stacks;
+        private int _lastCrateLine;
+
+        private string[] _lines;
+
+        public Day5()
+        {
+            _lines = File.ReadAllLines("Input/day5.txt");
+            Setup();
+        }
+
+        private void Setup()
+        {
+            _stacks = new CrateStack[(_lines[0].Length + 1) / 4];
+
+            _lastCrateLine = 0;
+            for (int i = 0; i < _lines.Length; i++)
+            {
+                string line = _lines[i];
+                if (line[1] == '1')
+                {
+                    _lastCrateLine = i;
+                    break;
+                }
+            }
+
+            for (int i = 0; i < _lastCrateLine; i++)
+            {
+                string line = _lines[i];
+                for (int j = 1; j < line.Length; j += 4)
+                {
+                    if (line[j] != ' ')
+                    {
+                        CrateStack stack = _stacks[j / 4];
+                        if (stack == null)
+                        {
+                            stack = new CrateStack(_lastCrateLine * _stacks.Length);
+                            _stacks[j / 4] = stack;
+                            stack.Size = _lastCrateLine - i;
+                        }
+
+                        stack.Stack[_lastCrateLine - i - 1] = line[j];
+                    }
+                }
+            }
+
+
+
+            if (_moves.Count == 0)
+            {
+                for (int i = 10; i < _lines.Length; i++)
+                {
+                    string line = _lines[i];
+                    string[] splits = line.SplitOn("move ", " from ", " to ");
+                    int amount = splits[1].ToInt();
+                    int from = splits[2].ToInt() - 1;
+                    int to = splits[3].ToInt() - 1;
+
+                    _moves.Add((amount, from, to));
+                }
+            }
+        }
+
+
+        public string PartOne()
+        {
+            for (int i = 0; i < _moves.Count; i++)
+            {
+                (int amount, int from, int to) = _moves[i];
+                _stacks[from].MoveTo(_stacks[to], amount);
+            }
+
+            return TopOfStacks();
+        }
+
+        public string PartTwo()
+        {
+            Setup();
+            for (int i = 0; i < _moves.Count; i++)
+            {
+                (int amount, int from, int to) = _moves[i];
+                _stacks[from].MoveToAtOnce(_stacks[to], amount);
+            }
+
+            return TopOfStacks();
+        }
+        private string TopOfStacks()
+        {
+            char[] result = new char[_stacks.Length];
+            for (int i = 0; i < _stacks.Length; i++)
+            {
+                result[i] = _stacks[i].Top;
+            }
+            return new string(result);
+        }
+
+        private class CrateStack
+        {
+            public char[] Stack;
+            public int Size;
+
+            public CrateStack(int maxSize)
+            {
+                Stack = new char[maxSize];
+            }
+            public char Top { get { return Stack[Size - 1]; } }
+            public void MoveTo(CrateStack target, int amount)
+            {
+                Size -= amount;
+                for (int i = Size + amount - 1; i >= Size; i--)
+                {
+                    target.Stack[target.Size++] = Stack[i];
+                }
+            }
+            public void MoveToAtOnce(CrateStack target, int amount)
+            {
+                for (int i = Size - amount; i < Size; i++)
+                {
+                    target.Stack[target.Size++] = Stack[i];
+                }
+                Size -= amount;
+            }
+            public override string ToString()
+            {
+                return $"Size={Size} Top={Top}";
+            }
+        }
+    }
+}
